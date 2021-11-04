d22021782a93356fef9039b1ccfa7660c4bca7e6
mbrav <mbrav@protonmail.com>
2021-11-04 19:30:57 +0300

    add basic models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Comment
+from .models import Category, Genres, Title, Review, Comment, Rating
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Genres)
+admin.site.register(Title)
+admin.site.register(Review)
 admin.site.register(Comment)
+admin.site.register(Rating)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,18 +3,87 @@ from datetime import date
 from users.models import User
 
 
-class Comment(models.Model):
-    text = models.TextField()
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя категории',
+    )
 
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Slug категории',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя жанра',
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Slug жанра',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+
+    name = models.TextField()
+    year = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        'Дата публикации ревью',
+        auto_now_add=True,
+        blank=True
+    )
+
+
+class Comment(models.Model):
+
+    text = models.TextField()
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='comments'
     )
-
     pub_date = models.DateTimeField(
-        'date published', auto_now_add=True, blank=True
+        'Дата публикации коммента',
+        auto_now_add=True,
+        blank=True
     )
 
     class Meta:
         ordering = ('-pub_date',)
+
+
+class Rating(models.Model):
+    text = models.TextField()
