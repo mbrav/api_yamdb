c12d8394aef7905e60a843614f777ee4ebfd39fa
mbrav <mbrav@protonmail.com>
2021-11-05 22:03:15 +0300

    removed unecessary code

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -138,3 +138,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -11,35 +10,30 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    username = serializers.CharField(
+        write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2',
-                  'email', 'first_name', 'last_name')
+        fields = ['username', 'email']
         extra_kwargs = {
-            'first_name': {'required': True},
-            'last_name': {'required': True}
+            'username': {'required': True},
+            'email': {'required': True}
         }
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if attrs['username'] == 'me':
             raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
+                {"username": "Username cannot be 'me'"})
 
         return attrs
 
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            email=validated_data['email']
         )
 
-        user.set_password(validated_data['password'])
         user.save()
 
         return user
