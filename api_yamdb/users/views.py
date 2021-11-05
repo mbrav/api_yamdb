468ea869ba922d4541c1d2e215f5e3066c8c5ecf
mbrav <mbrav@protonmail.com>
2021-11-06 00:32:47 +0300

    Setup email

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics, viewsets, status
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-
+from .utils import Util
+from django.core.mail import EmailMessage
 from .serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
@@ -23,6 +25,20 @@ class YamDBRegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+
+        email_body = 'Добрый день, ' + \
+            user.username + 'Ваш Токен: \n' + str(token)
+        data = {
+            'email_body': email_body,
+            'to_email': user.email,
+            'email_subject': 'Verify your email'
+        }
+        Util.send_email(data)
+
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
 
