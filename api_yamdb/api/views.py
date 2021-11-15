e99e8d86745a347048d5d1dff232a68f430d5180
mazila52 <yaserenkii@yandex.ru>
2021-11-16 01:43:50 +0300

    Review_16/11 01:31

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -130,10 +130,8 @@ class ReviewViewSet(viewsets.ModelViewSet):
         new_queryset = Review.objects.filter(title=title)
         return new_queryset
 
-    """ def perform_create(self, serializer):
-        print('1')
+    def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, id=title_id)
-        print('1')
-        serializer.save(author=self.request.user, title=title) """
+        serializer.save(author=self.request.user, title=title)
 
