2bd1bb686b9fcd71b312cd65456d2b9db6c8705b
mbrav <mbrav@protonmail.com>
2021-11-16 20:55:40 +0300

    ХОЛЛИ ФАКИНГ ШИТ! DONE!  #8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -152,16 +152,29 @@ class CommentViewSet(viewsets.ModelViewSet):
         review = self.review()
         serializer.save(author=self.request.user, review=review)
 
-    def perform_update(self, serializer):
+    def update(self, request, *args, **kwargs):
         user = self.req_user()
-        instance = self.get_object()
         auth = user.is_authenticated
         if not auth:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
         if user.role == 'user' and instance.author != user:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        serializer.save()
+
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
         # Если у юзера роль 'user' и он хочет удалить коммент
